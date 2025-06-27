b5ae52a0442f0449082aa5ef051a4cd72f81edd5
mrchiraggarg <gargchirag728@gmail.com>
2025-06-27 23:00:20 +0530

    Modified

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const DarkModeToggle = () => {
+type Props = {
+  dark: boolean,
+  setDark: (d: boolean) => void
+};
+
+export default function DarkModeToggle({ dark, setDark }: Props) {
   return (
-    <div>DarkModeToggle</div>
-  )
+    <button onClick={() => setDark(!dark)}>
+      {dark ? '🌙 Dark' : '☀️ Light'}
+    </button>
+  );
 }
-
-export default DarkModeToggle
